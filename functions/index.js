92885fb6bcb22cc0ac9d7520d0702760fe5c5a5a
wansom <ochiengwarren10@gmail.com>
2022-08-12 10:16:40 +0300

    set up initial express app

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const people = require("./routes/people");
+const auth =require('./routes/auth');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+//route handlers
+app.use("/auth", auth);
+app.use("/people", people);
+//error handlers
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message,
+  });
+});
+
+//app cloud function wrappers
+exports.barizi = functions.https.onRequest(app);
+
+
+

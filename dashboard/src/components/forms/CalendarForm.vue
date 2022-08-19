9bf9cff4f336f058322cbe6de382bfd993164613
wansom <ochiengwarren10@gmail.com>
2022-08-19 16:35:26 +0300

    forms

--- dashboard/src/components/forms/CalendarForm.vue
+++ dashboard/src/components/forms/CalendarForm.vue
@@ -18,19 +18,26 @@
 
         <a-form-item label="Company Type">
           <a-select default-value="monthly" @change="handleChange">
-      <a-select-option value="monthly">
-        Monthly
-      </a-select-option>
-      <a-select-option value="weekly">
-        Weekly
-      </a-select-option>
-      <a-select-option value="hourly">
-        Hourly
-      </a-select-option>
-    </a-select>
+            <a-select-option value="monthly"> Monthly </a-select-option>
+            <a-select-option value="weekly"> Weekly </a-select-option>
+            <a-select-option value="hourly"> Hourly </a-select-option>
+          </a-select>
         </a-form-item>
+        <a-form-item label="Department">
+          <a-select default-value="monthly" @change="handleChange">
+            <a-select-option value="monthly"> Sales </a-select-option>
+            <a-select-option value="weekly"> Engineering </a-select-option>
+            <a-select-option value="hourly"> legal </a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item label="Normal Payday">
+          <a-date-picker @change="onChange" />
+        </a-form-item>
+
         <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
-          <a-button type="primary" html-type="submit" :loading="loading"> Submit </a-button>
+          <a-button type="primary" html-type="submit" :loading="loading">
+            Submit
+          </a-button>
         </a-form-item>
       </a-form>
     </a-card>
@@ -38,61 +45,44 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 export default {
   data() {
     return {
       formLayout: "horizontal",
       form: this.$form.createForm(this, { name: "coordinated" }),
-      image:null,
+      image: null,
     };
   },
   methods: {
-    transformFile(file) {
-      return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-          const canvas = document.createElement("canvas");
-          const img = document.createElement("img");
-          img.src = reader.result;
-          img.onload = () => {
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0);
-            ctx.fillStyle = "red";
-            ctx.textBaseline = "middle";
-            ctx.fillText("Ant Design", 20, 20);
-            canvas.toBlob(resolve);
-          };
-        };
-      });
-    },
+ onChange(){},
+ handleChange(){},
     handleSubmit(e) {
       e.preventDefault();
       this.form.validateFields((err, values) => {
         if (!err) {
           console.log("Received values of form: ", values);
-          this.$store.dispatch("addClients",values).then(()=>{
-            if(!this.error){
-              this.form.resetFields()
+          this.$store.dispatch("addClients", values).then(() => {
+            if (!this.error) {
+              this.form.resetFields();
             }
-          })
+          });
         }
       });
     },
   },
-  computed:{
-    ...mapState['clients'],
-    loading(){
-      return this.$store.state.loading
+  computed: {
+    ...mapState["clients"],
+    loading() {
+      return this.$store.state.loading;
     },
-    error(){
-      return this.$store.state.error
-    }
+    error() {
+      return this.$store.state.error;
+    },
   },
-  mounted(){
-    this.$store.dispatch("getClients")
-  }
+  mounted() {
+    this.$store.dispatch("getClients");
+  },
 };
 </script>
 

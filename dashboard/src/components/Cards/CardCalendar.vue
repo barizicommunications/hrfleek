2721a06b6e7e3dce99c60ff4e7c16243eec18f60
wansom <ochiengwarren10@gmail.com>
2022-08-24 09:56:44 +0300

    view calendar

--- dashboard/src/components/Cards/CardCalendar.vue
+++ dashboard/src/components/Cards/CardCalendar.vue
@@ -1,5 +1,34 @@
 <template>
-  <a-calendar @select="onSelect">
+<div>
+
+      <a-modal v-model="visible" :title="selectedDate" on-ok="handleOk">
+      <template slot="footer">
+        <a-button key="back" @click="handleCancel">
+          Close
+        </a-button>
+       
+      </template>
+      	<a-card :bordered="false" class="card-info-2 h-full" v-for="selected in selectedCalendar" :key="selected.id">
+		<div class="col-content h-full" style="background-image: url('images/info-card-2.jpg')">
+			<div class="card-content">
+				<h5>{{selected.calendar_name}}</h5>
+				<p>Payment cycle:{{selected.payment_cycle}}</p>
+        <p>Payment Date:{{selected.payment_cycle}}</p>
+        <p>Department:{{selected.department}}</p>
+        <p>Payment date:{{selected.date.toDate().toLocaleDateString()}}</p>
+			</div>
+			<div class="card-footer">
+				<a-button size="small" type="primary">
+					<span>Create A payrun</span>
+					<svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M7.29289 14.7071C6.90237 14.3166 6.90237 13.6834 7.29289 13.2929L10.5858 10L7.29289 6.70711C6.90237 6.31658 6.90237 5.68342 7.29289 5.29289C7.68342 4.90237 8.31658 4.90237 8.70711 5.29289L12.7071 9.29289C13.0976 9.68342 13.0976 10.3166 12.7071 10.7071L8.70711 14.7071C8.31658 15.0976 7.68342 15.0976 7.29289 14.7071Z" fill="#111827"/>
+					</svg>
+				</a-button>
+			</div>
+		</div>
+	</a-card>
+    </a-modal>
+    <a-calendar @select="onSelect">
     <ul slot="dateCellRender" slot-scope="value" class="events">
       <li v-for="item in getListData(value)" :key="item.id">
         <a-badge status="success" :text="item.calendar_name" />
@@ -9,6 +38,7 @@
       <div v-if="getMonthData(value)" class="notes-month"></div>
     </template>
   </a-calendar>
+</div>
 </template>
 <script>
 import moment from "moment";
@@ -16,46 +46,42 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      setevents: [
-        {
-          date: moment("2012-08-15"),
-          type: "warning",
-          content: "meeting with ron.",
-        },
-        {
-          date: moment("2012-08-05"),
-          type: "success",
-          content: "Wedding at crystal.",
-        },
-        {
-          date: moment("2017-08-20"),
-          type: "success",
-          content: "moment with java.",
-        },
-      ],
+      selectedCalendar: [],
+      visible:false,
+      loading:false,
+      selectedDate:""
     };
   },
 
   methods: {
+    handleCancel(){
+this.visible =false
+    },
+    handleOk(){
+      this.visible =false
+    },
     onPanelChange(value, mode) {
       console.log(value, mode);
     },
     onSelect(value) {
-      let date = value.toDate();
+      let date = value.toDate().toLocaleDateString("en-US");
 
       let listData = this.calendars.filter(
-        (e) => e.date.toDate().getUTCDate() == date.getUTCDate()
+        (e) => e.date.toDate().toLocaleDateString("en-US") == date
       );
-      console.log(
-        date.toLocaleDateString("en-US"),
-        listData,
-        this.calendars[0].date.toDate().toLocaleDateString("en-US")
-      );
+      this.selectedCalendar = listData;
+      this.selectedDate=date
+      if(listData.length){
+        this.visible =true
+      }
     },
     getListData(value) {
       let listData = [];
       this.calendars.forEach((element) => {
-        if (value.toDate().toLocaleDateString("en-US")== element.date.toDate().toLocaleDateString("en-US")) {
+        if (
+          value.toDate().toLocaleDateString("en-US") ==
+          element.date.toDate().toLocaleDateString("en-US")
+        ) {
           listData.push(element);
         }
       });

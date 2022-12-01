b6c85259d1fc33496639a00f569a89d45f9b14b8
wansom <ochiengwarren10@gmail.com>
2022-12-01 07:52:55 +0300

    edit allowances

--- dashboard/src/components/forms/EditAllowances.vue
+++ dashboard/src/components/forms/EditAllowances.vue
@@ -4,6 +4,48 @@
     class="header-solid h-full px-5 py-10"
     :bodyStyle="{ paddingTop: 0, paddingBottom: 0 }"
   >
+  <a-modal v-model="visible" title="Add Allowance" @ok="handleSubmit">
+    <a-form
+        :form="form"
+        @submit.prevent="handleSubmit"
+      >
+        <a-form-item label="Allowance Name">
+          <a-input
+            v-decorator="[
+              'allowance_name',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]"
+          />
+        </a-form-item>
+        <a-form-item label="Allowance Taxed">
+          <a-select v-decorator="[
+              'taxed',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]">
+            <a-select-option value="yes">Yes </a-select-option>
+            <a-select-option value="no">No</a-select-option>
+           
+          </a-select>
+        </a-form-item>
+        <a-form-item label="Frequency">
+          <a-radio-group name="radioGroup"     v-decorator="[
+              'frequency',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]">
+            <a-radio value="oneoff"> One off </a-radio>
+            <a-radio value="monthly"> Monthly</a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <a-form-item label="Amount">
+          <a-input
+            v-decorator="[
+              'amount',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]"
+          />
+        </a-form-item>
+      </a-form>
+    </a-modal>
   <a-list :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }" :data-source="data.allowances">
     <a-list-item slot="renderItem" slot-scope="item, index">
       <a-card :title="item.name">
@@ -12,7 +54,7 @@
       </a-card>
     </a-list-item>
   </a-list>
-      <a-button type="primary" block icon="plus">
+      <a-button type="primary" block icon="plus" @click="handleOk">
         Add Allowance
       </a-button>
 </a-card>
@@ -21,7 +63,23 @@
 
 <script>
 export default {
-  props:['data']
+  props:['data'],
+  data(){
+    return{
+      visible:false,
+      formLayout: "vertical",
+      form: this.$form.createForm(this, { name: "coordinated" }),
+    }
+  },
+  methods:{
+    handleOk(){
+      this.visible=true
+    },
+    handleSubmit(){
+      
+    }
+
+  }
 
 }
 </script>
--- dashboard/src/components/forms/EditDeductions.vue
+++ dashboard/src/components/forms/EditDeductions.vue
@@ -4,6 +4,38 @@
     class="header-solid h-full px-5 py-10"
     :bodyStyle="{ paddingTop: 0, paddingBottom: 0 }"
   >
+  <a-modal v-model="visible" title="Add Deduction" @ok="handleSubmit">
+    <a-form
+        :form="form"
+        @submit.prevent="handleSubmit"
+      >
+        <a-form-item label="Deduction Name">
+          <a-input
+            v-decorator="[
+              'name',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]"
+          />
+        </a-form-item>
+        <a-form-item label="Frequency">
+          <a-radio-group name="radioGroup" :default-value="1"    v-decorator="[
+              'frequency',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]">
+            <a-radio value="oneoff"> One off </a-radio>
+            <a-radio value="monthly"> Monthly</a-radio>
+          </a-radio-group>
+        </a-form-item>
+        <a-form-item label="Amount">
+          <a-input
+            v-decorator="[
+              'amount',
+              { rules: [{ required: true, message: 'Field is required!' }] },
+            ]"
+          />
+        </a-form-item>
+      </a-form>
+  </a-modal>
   <a-list :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }" :data-source="data.deductions">
     <a-list-item slot="renderItem" slot-scope="item, index">
         
@@ -13,7 +45,7 @@
       </a-card>
     </a-list-item>
   </a-list>
-      <a-button type="primary" block icon="plus">
+      <a-button type="primary" block icon="plus" @click="handleOk">
         Add Deduction
       </a-button>
 </a-card>
@@ -22,7 +54,23 @@
 
 <script>
 export default {
-  props:['data']
+  props:['data'],
+  data(){
+    return{
+      visible:false,
+      formLayout: "vertical",
+      form: this.$form.createForm(this, { name: "coordinated" }),
+    }
+  },
+  methods:{
+    handleOk(){
+      this.visible=true
+    },
+    handleSubmit(){
+      
+    }
+
+  }
 
 }
 </script>

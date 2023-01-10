46a855f06766a996de916f15ff13b7d466ec7375
wansom <ochiengwarren10@gmail.com>
2023-01-10 15:58:40 +0300

    send payslip via email

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,6 +1,16 @@
 const admin = require("firebase-admin");
+const firebaseConfig={
+  apiKey: "AIzaSyDMsPInM58uPksNu3E9iQ5EhnWBz67CPeQ",
+  authDomain: "scanpal-f74da.firebaseapp.com",
+  databaseURL: "https://scanpal-f74da.firebaseio.com",
+  projectId: "scanpal-f74da",
+  storageBucket: "scanpal-f74da.appspot.com",
+  messagingSenderId: "702443834173",
+  appId: "1:702443834173:web:a4148356cf434a6fda1354",
+  measurementId: "G-37KL0M4ESD"
+};
 
-admin.initializeApp(process.env.firebaseConfig);
+admin.initializeApp(firebaseConfig);
 
 
 // utils
--- functions/routes/mail.js
+++ functions/routes/mail.js
@@ -1,39 +1,89 @@
-const express =require("express");
+const express = require("express");
 const nodemailer = require("nodemailer");
-const router =express.Router()
+const router = express.Router();
+const { createPDF } = require("./createPDF");
+const fs = require("fs");
+const handlebars = require("handlebars");
+const path = require("path");
 
-router.post('/',(req,res,next)=>{
-    const sendMail =async()=>{
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "wochieng@barizicommunications.com", // generated ethereal user
-      pass: "Qur27861", // generated ethereal password
-    },
+router.post("/", (req, res, next) => {
+  const sendMail = async () => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "wochieng@barizicommunications.com", // generated ethereal user
+        pass: "Yaw61537", // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    const mailOptions = {
+      from: "wochieng@barizicommunications.com", // sender address
+      to: "ochiengwarren10@gmail.com", // list of receivers
+      subject: "Test Email", // Subject line
+      html: '<p style="color:red;">Your html here</p>', // plain text body
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) return next(err);
+      else res.status(201).send(info);
+    });
+  };
+
+  return sendMail();
+});
+
+let emailClient = nodemailer.createTransport({
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: "wochieng@barizicommunications.com", // generated ethereal user
+    pass: "Yaw61537", // generated ethereal password
+  },
+});
+
+const createHTMLToSend = (path, replacements) => {
+  let html = fs.readFileSync(path, {
+    encoding: "utf-8",
   });
+  let template = handlebars.compile(html);
 
-  // send mail with defined transport object
-   const mailOptions = {
-     from: 'wochieng@barizicommunications.com', // sender address
-     to: 'ochiengwarren10@gmail.com', // list of receivers
-     subject: 'Test Email', // Subject line
-     html: '<p style="color:red;">Your html here</p>'// plain text body
-   };
+  let htmlToSend = template(replacements);
 
-   transporter.sendMail(mailOptions, function (err, info) {
-      if(err)
-        return next(err)
-      else
-        res.status(201).send(info)
+  return htmlToSend;
+};
 
-   });
-    }
+router.post('/send',(req,res,next)=>{
+  const sendPDF = async () => {
+    const emailPath = path.resolve("./routes/email-templates", "payslip.html");
+  
+    const replacements = {
+      name: "Warren",
+      date: new Date().toDateString(),
+    };
+  
+    let htmlToSend = createHTMLToSend(emailPath, replacements);
+    let pdfOutput = await createPDF();
 
-    return sendMail()
+    const mailOptions = {
+      from: "wochieng@barizicommunications.com", // sender address
+      to: "ochiengwarren10@gmail.com", // list of receivers
+      subject: "Your Payslip", // Subject line
+      html: htmlToSend, // plain text body
+      attachments: [{ path: pdfOutput }],
+    };
+  
+  emailClient.sendMail(mailOptions, function (err, info) {
+    if (err) return next(err);
+    else res.status(201).send(info);
+  });
+  };
+  return sendPDF()
 
 })
 
-module.exports =router
+module.exports = router;
